2b236d4a7e9a510d9df23e2a52945b82fd08cd58
tanasi111 <ivantanasi111@gmail.com>
2019-08-28 15:35:40 +0200

    dev

--- src/index.js
+++ src/index.js
@@ -112,7 +112,7 @@ class Game extends React.Component {
                 <div className="game-board">
                     <Board
                         squares={current.squares}
-                        onCLick={i => this.handleClick(i)}
+                        onClick={i => this.handleClick(i)}
                     />
                 </div>
                 <div className="game-info">
